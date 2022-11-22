884e250a66043009e65a7dd3368897cf9d2705db
Patrick Cable <p.cable@f5.com>
2022-11-22 08:37:00 -0500

    Copyright/Clearer License

--- src/main/java/com/threatstack/cassandra/db/compaction/MinTimeCompactionStrategy.java
+++ src/main/java/com/threatstack/cassandra/db/compaction/MinTimeCompactionStrategy.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2015-2022 F5 Inc.
+ * 
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
